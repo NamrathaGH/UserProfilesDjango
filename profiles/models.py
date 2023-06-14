327493c39ea0de4ee92325d58d5591be1241945e
NamrathaGH <90101043+NamrathaGH@users.noreply.github.com>
2023-06-14 11:09:56 +0530

    Create models.py

---
+++ profiles/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
